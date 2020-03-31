1466547bc42946e27e19882f8a4096dc5010ce13
hojin-kr <jhj377@gmail.com>
2020-04-01 00:04:43 +0900

    조회api

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,25 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/users', function (Request $request) {
-  return DB::select('select * from r6s');
-});
+Route::get('/get/user/{name}/', function ($name) {
 
-Route::get('/users', function (Request $request) {
-  return DB::select('select * from r6s');
-});
+  $userSql = DB::select('select * from r6s where nickname = ? order by id desc', [$name]);
+  //mysql 갱신한지 최근이면 바로 리턴
+  if (!empty($userSql) && $userSql[0]->update_time > (time() - 300)) {
+    return $userSql;
+  }
 
-Route::get('/stage/{userId}/{stageId}', 'StageCall@stage');
+  $getUser = file_get_contents("http://localhost:8001/getUser.php?name=".$name."&platform=uplay&appcode=r6s_api");
+  $row = json_decode($getUser, true);
+  $profile_id = array_keys($row['players'])[0];
+  $user = $row['players'][$profile_id];
+  $keys = array_keys($user);
+
+  $user['rank_image'] = $user['rankInfo']['image'];
+  $user['rank_name']  = $user['rankInfo']['name'];
+  $user['update_time'] = time();
+  unset($user['rankInfo']);
+  DB::table('r6s')->insert($user);
+
+  return DB::select('select * from r6s where nickname = ? order by id desc', [$name]);
+});
