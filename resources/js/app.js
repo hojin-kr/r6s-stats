c4f1485f572061a0488a1bf176a9cbbecbd791f6
hojin-kr <jhj377@gmail.com>
2020-06-18 00:33:43 +0900

    view

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,2 @@
 require('./bootstrap');
+require('./anime/lib/anime.min.js');
--- resources/views/main.php
+++ resources/views/main.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=0.73">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"">
     <link href="https://fonts.googleapis.com/css?family=Francois+One&display=swap" rel="stylesheet">
     <script src="https://d3js.org/d3.v5.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
@@ -23,7 +23,7 @@
         border-radius: 15px;
         opacity: 0.85;
         display: flex;
-        flex-direction: row;
+        flex-direction: column;
         justify-content: space-between;
 
       }
@@ -42,35 +42,90 @@
       }
 
       .search {
-       height: 3em;
-       width: 100%;
+       height: 2em;
        background-color: #292c33;
        border-radius: 15px;
        opacity: 0.65;
        border: none;
        color: #f5f5f5;
       }
-      .search input {
+      .search > input{
+       width: 100%;
+       height: 100%;
        background-color: #292c33;
-       width: 90%;
-       height: 80%;
-       margin : 0.3em;
-       border-radius: 15px;
-       opacity: 0.65;
        border: none;
-       color: #f5f5f5;
        font-size: 1em;
+       color: #f5f5f5;
+      }
+      .search > .progress {
+        position: absolute;
+        right: 45%;
+        top: 40%;
+        display: none;
       }
       .title{
         color: #fbc900;
       }
+      hr {
+        border-color: #f5f5f5;
+      }
+      .more {
+        color: #f5f5f5;
+      }
+      p {
+            margin-block-start: 0.3em;
+            margin-block-end: 0.3em;
+        }
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
     </style>
   </head>
   <body>
-    <div class='container'>
-        <input id="search" class="search" type="text" name="nickname" value="" placeholder="Search...">
+    <div class='container search'>
+        <input id="search" type="text" name="nickname" value="" placeholder="Search..."><span class="progress">Search...</span>
     </div>
-    <div id="content"></div>
+    <div id='content'>
+      <div class='container intro'>
+        <div class='wrap'>
+          <p class='title'>레식 전적 검색</p>
+          <p>아시아 계정의 닉네임을 검색</p>
+          <p>나의 랭킹과 오퍼레이터별 정보를 확인</p>
+          <p>지속 관리로 오퍼레이터별 변화를 모니터링</p>
+        </div>
+        <div class="more">
+            <hr>
+            <p class="btn">더보기</p>
+        </div>
+      </div>
+      <div class='container'>
+        <div class='wrap'>
+          <p class='title'>대회 정보</p>
+          <p>레인보우식스 시즈 대회 정보</p>
+          <p><a href="https://google.com">레식 대회 정보 가나다라마바사아차카타...</a></p>
+          <p class="title">[ 06/19 19:00 XXCUP XX MATCH ]</p>
+          <p>[img] A TEAM VS B TEAM [img]</p>
+          <p>[img] A TEAM VS B TEAM [img]</p>
+          <p class="title">[ 06/22 19:00 XXCUP XX MATCH ]</p>
+          <p>[img] A TEAM VS B TEAM [img]</p>
+        </div>
+        <div class="more">
+            <hr>
+            <p class="btn">더보기</p>
+        </div>
+      </div>
+      <div class='container'>
+        <div class='wrap'>
+          <p class='title'>메인 레식 정보</p>
+          <p>레인보우식스 시즈 최신 정보</p>
+          <p><a href="https://google.com">레식 정보 가나다라마바사아차카타...</a></p>
+          <p><a href="https://google.com">레식 정보 가나다라마바사아차카타...</a></p>
+          <p><a href="https://google.com">레식 정보 가나다라마바사아차카타...</a></p>
+          <p><a href="https://google.com">레식 정보 가나다라마바사아차카타...</a></p>
+        </div>
+      </div>
+    </div>
   </body>
 </html>
 
@@ -81,8 +136,14 @@ $('#search').on('change', ()=>{
   search($('#search').val());
 })
 
+// 더보기 버튼
+$('.more > .btn').on('click', ()=>{
+  alert("more");
+})
+
 function search(nickname) {
   $('body #content').html('');
+  $('.search > .progress').css('display', 'block');
     $.ajax({
             method: "POST",
             url: "api/get/id",
@@ -100,6 +161,7 @@ function search(nickname) {
 }
 
 function getProfile(id) {
+    
     $.ajax({
             method: "POST",
             url: "api/get/profile",
@@ -153,25 +215,29 @@ function addContainer(data, type) {
   let dom = '';
   switch(type) {
     case 0:
-      dom = "<div class='container'> \
-      <div class='overview'> \
+      dom = "<div class='container profile'> \
+      <div class='wrap'> \
       <span class='title'>[ "+ data.nickname +" ]</span><br> \
       Level "+ data.level +"<br> \
       </div> \
     </div>";
     break;
     case 1:
-      dom = "<div class='container'> \
-      <div class='overview'> \
+      dom = "<div class='container rank'> \
+      <div class='wrap'> \
       <span class='title'>[ Rank "+ data.rank +" ]</span><br> \
       MMR "+ data.mmr +"<br> \
       SEASON "+ data.season +"<br> \
       </div> \
+      <div class='more'> \
+            <hr> \
+            <p class='btn'>더보기</p> \
+        </div> \
     </div>";
     break;
     case 2:
-      dom = "<div class='container'> \
-      <div class='overview'> \
+      dom = "<div class='container operators'> \
+      <div class='wrap'> \
       <span class='title'>[ "+ data.operator +" ]</span><br> \
       Win "+ data.roundwon +"<br> \
       Lost "+ data.roundlost +"<br> \
@@ -179,9 +245,14 @@ function addContainer(data, type) {
       Death "+ data.death +"<br> \
       TimePlayed "+ data.timeplayed +"<br> \
       </div> \
+      <div class='more'> \
+            <hr> \
+            <p class='btn'>더보기</p> \
+        </div> \
     </div>";
     break;
   }
+  $('.search > .progress').css('display', 'none');
     $('body #content').append(dom);
 }
 </script>
